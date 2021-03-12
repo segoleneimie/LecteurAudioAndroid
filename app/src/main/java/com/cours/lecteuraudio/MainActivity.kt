5c229283a49683804105fb588dd59b987b2149fb
segoleneimie <segolene.vincent@imie.fr>
2021-03-12 17:01:44 +0100

    recycler view et creation de la class musiques

--- app/src/main/java/com/cours/lecteuraudio/MainActivity.kt
+++ app/src/main/java/com/cours/lecteuraudio/MainActivity.kt
@@ -2,10 +2,31 @@ package com.cours.lecteuraudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // à ajouter pour de meilleures performances :
+        liste_musiques.setHasFixedSize(true)
+
+        // layout manager, décrivant comment les items sont disposés :
+        val layoutManager = LinearLayoutManager(this)
+        liste_musiques.layoutManager = layoutManager
+        // contenu d'exemple :
+        val listeMusique: MutableList<Musique> = ArrayList()
+        for (i in 0..50){
+            listeMusique.add(Musique("Pommes", "30Mo", "3min"))
+            listeMusique.add(Musique("Poires", "30Mo", "3min"))
+        }
+
+        // adapter :
+        val musiquesAdapter = MusiquesAdapter(listeMusique)
+        liste_musiques.adapter = musiquesAdapter
     }
 }
---
+++ app/src/main/java/com/cours/lecteuraudio/Musique.kt
@@ -0,0 +1,4 @@
+package com.cours.lecteuraudio
+
+class Musique(var titre: String, var taille: String, var duree: String) {
+}
